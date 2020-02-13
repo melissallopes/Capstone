dfd80b786afa3ab819ffa33225d7f25cb5671d7f
Melissa Lima Lopes <melissalimalopes@Melissas-MBP.hitronhub.home>
2020-02-13 10:24:06 -0800

    track package.json

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -12,6 +12,13 @@ app.use(cors());
 app.use("/api/exercises", require("./routes/api/exercises"));
 app.use("/api/user", require("./routes/api/user"));
 
-app.listen(PORT, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
   console.log("listening");
-});
+}
+app.listen(port);
+
+// app.listen(PORT, () => {
+//   console.log("listening");
+// });
